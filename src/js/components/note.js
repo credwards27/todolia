ba84cfbe715a0259a76ec5f42fcd9f8801d504b8
credwards27 <credwards27@users.noreply.github.com>
2017-08-01 00:40:47 -0400

    Added empty Note class

---
+++ src/js/components/note.js
@@ -0,0 +1,34 @@
+/* note.js
+    
+    Note clase object.
+*/
+
+/* Note object.
+*/
+class Note {
+    //
+    // STATIC PROPERTIES
+    //
+    
+    //
+    // PROPERTIES
+    //
+    
+    /* Constructor for Note.
+    */
+    constructor() {}
+    
+    /* Destructor for Note.
+    */
+    destroy() {}
+    
+    //
+    // STATIC METHODS
+    //
+    
+    //
+    // METHODS
+    //
+}
+
+module.exports = Note;
