515270ddfb07f551038ed62cb8a67c9cdf326253
credwards27 <credwards27@users.noreply.github.com>
2017-08-01 02:59:47 -0400

    Added mock data to Note class

--- src/js/components/note.js
+++ src/js/components/note.js
@@ -19,6 +19,37 @@ class Note {
     // Unique note ID name.
     _id = null;
     
+    // Note content nodes.
+    _data = [
+        "This is free text. It can be freely edited with formatting.",
+        
+        "Text can be <strong>strong</strong>, <em>emphasized</em>, " +
+            "<span class='u'>underlined</span>, and <span " +
+            "class='s'>crossed out</span>.",
+        
+        "You can also <strong>mix <em>and<em> match</strong> " +
+            "formatting. It doesn't <span class='u'>have to be <span " +
+            "class='s'>perfect</span></span>",
+        
+        "Finally, you can also have <a " +
+            "href='http://example.com'>links</a>",
+        
+        {
+            checked: false,
+            text: "Most importantly, you can have checklists!"
+        },
+        
+        {
+            checked: false,
+            text: "They can be <strong>formatted!</strong>"
+        },
+        
+        {
+            checked: true,
+            text: "This one's finished!"
+        }
+    ];
+    
     /* Constructor for Note.
         
         id - Note ID name.
