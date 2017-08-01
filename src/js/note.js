b1c9978305316239232df366345bdefaecfe9bad
credwards27 <credwards27@users.noreply.github.com>
2017-08-01 00:39:06 -0400

    Added empty entry point script and stylesheet for note view

---
+++ src/js/note.js
@@ -0,0 +1,15 @@
+/* note.js
+    
+    Entry point script for note view.
+*/
+
+"use strict";
+
+const Note = require("~/components/note"),
+    $ = window.jQuery;
+
+/* Document ready initializer.
+*/
+function init() {}
+
+$(init);
