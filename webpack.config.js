e240d1e26b43b67bbba94da764ebdb6f07b8be6a
credwards27 <credwards27@users.noreply.github.com>
2018-01-21 00:28:24 -0500

    Added babel-plugin-transform-es2015-arrow-functions module to properly handle 'this' in arrow functions when transpiled

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,9 @@ module.exports = {
                     plugins: [
                         "transform-es2015-parameters",
                         "transform-class-properties",
+                        [ "transform-es2015-arrow-functions", {
+                            spec: true
+                        } ],
                         [ "babel-plugin-transform-builtin-extend", {
                             globals: [ "Error" ]
                         } ],
