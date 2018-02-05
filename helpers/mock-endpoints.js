9b1c63ca00e428435a95daf7eab43568ee9b36b8
credwards27 <credwards27@users.noreply.github.com>
2018-02-04 20:05:21 -0500

    Implemented mock handler for saving note data to disk

--- helpers/mock-endpoints.js
+++ helpers/mock-endpoints.js
@@ -4,10 +4,89 @@
 */
 
 // Dependencies.
-const server = require("./mock-server");
+const fs = require("fs"),
+    server = require("./mock-server");
 
 // Directory path for data files (with trailing slash).
-const DATA_PATH = process.env.HOME + "/Library/Application Support/";
+const DATA_PATH = process.env.HOME + "/Library/Application Support/Todolia/";
+
+/* Initializes persistent storage if necessary.
+*/
+function initPersistentStorage() {
+    var dirs = [
+        DATA_PATH,
+        DATA_PATH + "notes"
+    ],
+        path, stat, i, l;
+    
+    for (i=0, l=dirs.length; i<l; ++i) {
+        path = dirs[i];
+        
+        // Create directory if necessary
+        if (!fs.existsSync(path)) {
+            fs.mkdirSync(path);
+            continue;
+        }
+        
+        // If path exists, make sure it's actually a directory
+        stat = fs.statSync(path);
+        
+        if (!stat.isDirectory()) {
+            return {
+                code: "non-directory",
+                error: "'" + path.replace(/\/+$/, "") + "' exists, but is " +
+                    "not a directory"
+            };
+        }
+    }
+}
+
+
+/* Writes data to a note file.
+    
+    id - Note ID.
+    data - Data to write.
+    success - Success callback.
+    error - Error callback. First argument is an error object.
+*/
+function saveNote(id, content, settings, success, error) {
+    // Exit if note ID is invalid
+    if (typeof id !== "string" && typeof id !== "number") {
+        error({
+            status: 400,
+            error: "Invalid ID '" + id + "' provided for note"
+        });
+        
+        return;
+    }
+    
+    var data = {
+        id: id,
+        content: content || "",
+        settings: typeof settings === "object" ? settings : {}
+    };
+    
+    // Attempt to write to file
+    fs.writeFile(
+        DATA_PATH + "notes/" + id + ".json",
+        JSON.stringify(data),
+        {
+            mode: 0o644
+        },
+        (err) => {
+            if (err) {
+                error({
+                    status: 500,
+                    error: "Note '" + id + "' could not be saved"
+                });
+                
+                return;
+            }
+            
+            success();
+        }
+    );
+}
 
 //
 // Register default route handlers
@@ -34,6 +113,7 @@ server.registerRoute("save", function(req, res) {
         res.statusCode = 200;
         res.setHeader("Content-Type", "application/json");
         
+        // Parse the request data
         try {
             data = JSON.parse(body);
         }
@@ -48,8 +128,27 @@ server.registerRoute("save", function(req, res) {
             return;
         }
         
-        res.end(JSON.stringify("Saving:\n" + body));
+        // Make sure note ID is present
+        if (typeof data.id !== "string" && typeof data.id !== "number") {
+            res.statusCode = 400;
+            
+            res.end(JSON.stringify({
+                status: 400,
+                error: "No note ID provided"
+            }));
+            
+            return;
+        }
+        
+        // Save the note to disk
+        saveNote(data.id, data.content, data.settings, () => {
+            res.end(JSON.stringify("Note '" + data.id + "' saved"));
+        }, (e) => {
+            res.end(JSON.stringify(e));
+        });
     });
 }, "post");
+
+initPersistentStorage();
 
 module.exports = server;
